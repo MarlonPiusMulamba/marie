41bc4c272348475259982cb464b0ff368a115bda
Mulamba Pius <127339680+MarlonPiusMulamba@users.noreply.github.com>
2025-07-07 18:46:04 +0300

    aaaaaaaaa

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const port = 3000;
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 // Define routes
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
 });
+
+
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
