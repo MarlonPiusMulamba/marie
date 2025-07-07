c1d370426348507f5e42e023d75b0d72d0eb2f97
Mulamba Pius <127339680+MarlonPiusMulamba@users.noreply.github.com>
2025-07-07 18:28:23 +0300

    intitial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+
+// Define routes
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
